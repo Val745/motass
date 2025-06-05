a843741e7bff6a9a08588227a0c46d67d78fe0d3
Val745 <valerievidesdiaz@gmail.com>
2025-06-05 00:12:33 -0500

    Agrega migraciones iniciales

--- motas/settings.py
+++ motas/settings.py
@@ -2,7 +2,9 @@
 from pathlib import Path
 import os
 import dj_database_url
-
+from dotenv import load_dotenv
+load_dotenv()
+# ...existing code...
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
