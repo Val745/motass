37c839cde29c9ebf84175eb6677d04b4a323cdf5
Val745 <valerievidesdiaz@gmail.com>
2025-06-04 23:37:00 -0500

    url bd por defecto

--- motas/settings.py
+++ motas/settings.py
@@ -5,7 +5,7 @@ import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parentDEBUG = False 
+BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 APPEND_SLASH = True
